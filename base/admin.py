c2f2784793f3826e4c37b4ccee097f3ccbf3c25d
edualt <eduardosebastianhdz503@gmail.com>
2022-02-01 13:03:53 -0600

    first commit

---
+++ base/admin.py
@@ -0,0 +1,8 @@
+from asyncio import Task
+from django.contrib import admin
+from .models import Task
+
+admin.site.register(Task)
+
+
+
---
+++ base/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from base.models import Task
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),#Login
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),#logout
+    path('register/', RegisterPage.as_view(), name='register'),
+
+    path('', TaskList.as_view(), name='tasks'),#All created tasks
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),#Specified task
+    path('task-create/', TaskCreate.as_view(), name='task-create'),#Create task
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),#Update task
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),#Delete task
+]
