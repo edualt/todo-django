109da4aa9587027b0c9450d2c4b6309395ada64f
edualt <eduardosebastianhdz503@gmail.com>
2022-03-08 23:15:21 -0600

    New update

--- base/models.py
+++ base/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to="myimage", null=True, blank=True)
     complete = models.BooleanField(default=False)
