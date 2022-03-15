09da30e49ed4f69d0200cfe77c25df8cd82611ce
edualt <eduardosebastianhdz503@gmail.com>
2022-03-15 14:23:36 -0600

    i

--- todo/settings.py
+++ todo/settings.py
@@ -94,6 +94,9 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'customauth.MyUser'
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
