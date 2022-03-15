daeb78a0a0497c5e917935a4d9c8c73f9e3a310e
edualt <eduardosebastianhdz503@gmail.com>
2022-03-15 14:44:56 -0600

    i

--- todo/settings.py
+++ todo/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['arcane-dusk-50732.herokuapp.com']
+ALLOWED_HOSTS = ['arcane-dusk-50732.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
