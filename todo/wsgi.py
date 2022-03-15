a5adb7c224bfe3e9d75658077bea02b7939ce4ba
edualt <eduardosebastianhdz503@gmail.com>
2022-03-15 13:59:33 -0600

    i

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -16,7 +16,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
 
 application = get_wsgi_application()
 
-
-
 from whitenoise.django import DjangoWhiteNoise  
 application = DjangoWhiteNoise(application)
