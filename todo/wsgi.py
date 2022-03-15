e655afca333b4bf76a163a109f427508342851f5
edualt <eduardosebastianhdz503@gmail.com>
2022-03-15 13:55:52 -0600

    i

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -7,6 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -15,7 +17,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
 
 application = get_wsgi_application()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
+
 
 from whitenoise.django import DjangoWhiteNoise  
 application = DjangoWhiteNoise(application)
