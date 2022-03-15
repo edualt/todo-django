3bbb32d4314254f9bae5ee8837b0b315aaf55674
edualt <eduardosebastianhdz503@gmail.com>
2022-03-15 14:06:31 -0600

    i

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -6,15 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
-import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise  
-application = DjangoWhiteNoise(application)
