563280b0b2e67a70ded6f411fdb8d676267e67bf
edualt <eduardosebastianhdz503@gmail.com>
2022-03-15 14:03:35 -0600

    i

--- todo/settings.py
+++ todo/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
