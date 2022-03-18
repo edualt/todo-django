49f3dffe20507bd807ada221c28447287d212664
edualt <eduardosebastianhdz503@gmail.com>
2022-03-18 11:10:11 -0600

    new heroku name

--- todo/settings.py
+++ todo/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['arcane-dusk-50732.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['todo-app-ed.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
